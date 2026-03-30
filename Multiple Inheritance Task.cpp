76acb66e66e86d826c1dda8bad0082c8aee02ba6
Shanza Munir Mughal. <shanzamugal33@gmail.com>
2026-03-30 15:30:18 +0000

    Add files via upload

---
+++ Multiple Inheritance Task.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+class Printer
+{
+public:
+	void print_document()
+	{
+		cout<< "Printing Document!"<<endl;
+		}	
+};
+
+class Scanner
+{
+public:
+		void scan_document()
+		{
+			cout<<"Scanning document!"<<endl;
+		}
+};
+
+class Photocopier : public Printer, public Scanner
+{
+public:
+	void photocopy()
+	{
+	cout<<"Photocopy process started:"<<endl;
+	cout<<""<<endl;
+	scan_document();
+	print_document();
+	cout<<"Photocopy completed."<<endl;
+	}
+};
+int main()
+{
+	Photocopier p;
+	
+	cout<<"... Using Photocopier as Printer...   "<<endl;
+	p.print_document();
+	
+	cout<<" ... Using Photocopier as Scanner...  "<<endl;
+	p.scan_document();
+	
+	cout<<" ... Using Photocopier to Photocopy...   "<<endl;
+	p.photocopy();	
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
